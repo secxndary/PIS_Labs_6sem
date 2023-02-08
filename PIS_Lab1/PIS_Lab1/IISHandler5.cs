ac6a183bf9224f9c299489cdd2ab3b46e12aaecb
secxndary <valdaitsevv@mail.ru>
2023-02-09 00:14:07 +0300

    Lab 1 full refactor FrontEnd (added bootstrap v5)

--- PIS_Lab1/PIS_Lab1/IISHandler5.cs
+++ PIS_Lab1/PIS_Lab1/IISHandler5.cs
@@ -6,8 +6,6 @@ namespace PIS_Lab1
 {
     public class IISHandler5 : IHttpHandler
     {
-        #region Члены IHttpHandler
-
         public bool IsReusable
         {
             get { return true; }
@@ -22,24 +20,30 @@ namespace PIS_Lab1
             if (req.HttpMethod == "GET")
             {
                 res.ContentType = "text/html";
-                res.WriteFile("Task4.html");
+                res.WriteFile("Task5.html");
             }
             else if (req.HttpMethod == "POST")
             {
-
+                try
+                {
+                    var x = int.Parse(req.Form["x"]);
+                    var y = int.Parse(req.Form["y"]);
+                    var mul = x * y;
+                    res.Write(mul);
+                }
+                catch
+                {
+                    res.StatusCode = 400;
+                    res.AddHeader("Content-Type", "text/html");
+                    res.Write("<h2>X and Y parameters are not provided.<h2>");
+                }
             }
             else
             {
-
+                res.StatusCode = 405;
+                res.AddHeader("Content-Type", "text/html");
+                res.Write("<h2>Incorrect method.</h2>");
             }
-
-
-            //var x = int.Parse(req.Form["x"]);
-            //var y = int.Parse(req.Form["y"]);
-            //var sum = x + y;
-            //res.Write($"<h2>{x} + {y} = {sum}</h2>");
         }
-
-        #endregion
     }
 }
