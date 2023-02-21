1b7092fd845ec59891178e9f7eb26281d9d7ba16
secxndary <valdaitsevv@mail.ru>
2023-02-21 15:48:01 +0300

    Lab 4 scratch

---
+++ PIS_Lab4/PIS_Lab4/Models/PhoneBook.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PIS_Lab4.Models
+{
+    public class PhoneBook
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Surname is required.")]
+        public string Surname { get; set; }
+
+        [Phone]
+        [Required(ErrorMessage = "Phone Number is required.")]
+        [RegularExpression("^+375[2349]{2}[0-9]{7}$", ErrorMessage = "Phone number must match the pattern.")]
+        public string PhoneNumber { get; set; }
+    }
+}
