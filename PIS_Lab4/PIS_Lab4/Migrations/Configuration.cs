afb1745cb2b30439a7c6d98744841eaf38ba9ea3
secxndary <valdaitsevv@mail.ru>
2023-02-21 17:11:00 +0300

    попытка сделать миграцию в 4 лабе (неудачная)

---
+++ PIS_Lab4/PIS_Lab4/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace PIS_Lab4.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<PIS_Lab4.Context.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(PIS_Lab4.Context.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
