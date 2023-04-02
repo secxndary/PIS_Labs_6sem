88aee1b0b0c80efc8a211a283eef445b223648f4
secxndary <valdaitsevv@mail.ru>
2023-04-02 23:09:16 +0300

    Lab 5b Task 1

---
+++ PIS_Lab5b/PIS_Lab5b/App_Start/RouteConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace PIS_Lab5b
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapMvcAttributeRoutes();
+
+            routes.MapRoute("MXX", "MResearch/MXX", new { controller = "MResearch", action = "MXX" });
+        }
+    }
+}
