079a6be4cba9f0ba234d3a9c50dcb5bba0c455ed
secxndary <valdaitsevv@mail.ru>
2023-04-02 16:18:06 +0300

    5а лаба (гавно)

--- PIS_Lab5a/PIS_Lab5a/App_Start/RouteConfig.cs
+++ PIS_Lab5a/PIS_Lab5a/App_Start/RouteConfig.cs
@@ -8,9 +8,9 @@ namespace PIS_Lab5a
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
             routes.MapMvcAttributeRoutes();
 
+            // ===================================  MResearch: M01  ===================================
             routes.MapRoute("M01_1", "", new { controller = "MResearch", action = "M01" });
             routes.MapRoute("M01_2", "MResearch", new { controller = "MResearch", action = "M01" });
             routes.MapRoute("M01_3", "MResearch/M01", new { controller = "MResearch", action = "M01" });
@@ -18,16 +18,23 @@ namespace PIS_Lab5a
             routes.MapRoute("M01_5", "V2/MResearch/M01", new { controller = "MResearch", action = "M01" });
             routes.MapRoute("M01_6", "V3/MResearch/X/M01", new { controller = "MResearch", action = "M01" });
 
+            // ===================================  MResearch: M02  ===================================
             routes.MapRoute("M02_1", "V2", new { controller = "MResearch", action = "M02" });
             routes.MapRoute("M02_2", "V2/MResearch", new { controller = "MResearch", action = "M02" });
             routes.MapRoute("M02_3", "MResearch/M02", new { controller = "MResearch", action = "M02" });
             routes.MapRoute("M02_4", "V2/MResearch/M02", new { controller = "MResearch", action = "M02" });
             routes.MapRoute("M02_5", "V3/MResearch/X/M02", new { controller = "MResearch", action = "M02" });
 
+            // ===================================  MResearch: M03  ===================================
             routes.MapRoute("M03_1", "V3", new { controller = "MResearch", action = "M03" });
             routes.MapRoute("M03_2", "V3/MResearch/X", new { controller = "MResearch", action = "M03" });
             routes.MapRoute("M03_3", "V3/MResearch/X/M03", new { controller = "MResearch", action = "M03" });
 
+            // ======================================  CResearch  =====================================
+            routes.MapRoute("C01", "CResearch/C01", new { controller = "CResearch", action = "C01" });
+            routes.MapRoute("C02", "CResearch/C02", new { controller = "CResearch", action = "C02" });
+
+            // ======================================  404 Error  =====================================
             routes.MapRoute("MXX", "MResearch/MXX", new { controller = "MResearch", action = "MXX" });
         }
     }
