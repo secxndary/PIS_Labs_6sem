ce3450c36ebb3587fbd6a1dc37ab5e3b4334a7a6
secxndary <valdaitsevv@mail.ru>
2023-04-05 19:17:00 +0300

    Lab 5b full

--- PIS_Lab5b/PIS_Lab5b/App_Start/RouteConfig.cs
+++ PIS_Lab5b/PIS_Lab5b/App_Start/RouteConfig.cs
@@ -14,6 +14,9 @@ namespace PIS_Lab5b
             routes.MapRoute("AReserach_Action", "AA", new { controller = "AResearch", action = "AA" });
             routes.MapRoute("AReserach_Result", "AR", new { controller = "AResearch", action = "AR" });
             routes.MapRoute("AReserach_Exception", "AE", new { controller = "AResearch", action = "AE" });
+            
+            routes.MapRoute("CHReserach_AD", "AD", new { controller = "CHResearch", action = "AD" });
+            routes.MapRoute("CHReserach_AP", "AP", new { controller = "CHResearch", action = "AP" });
 
             routes.MapRoute("MXX", "MResearch/MXX", new { controller = "MResearch", action = "MXX" });
         }
