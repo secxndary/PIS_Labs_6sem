b90f1815912ba4228078d6c0e38ddeb4ee3f437f
secxndary <valdaitsevv@mail.ru>
2023-04-20 23:45:21 +0300

    lil refactor 5b, mail still not working

--- PIS_Lab5b/PIS_Lab5b/App_Start/RouteConfig.cs
+++ PIS_Lab5b/PIS_Lab5b/App_Start/RouteConfig.cs
@@ -8,16 +8,9 @@ namespace PIS_Lab5b
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
             routes.MapMvcAttributeRoutes();
 
-            routes.MapRoute("AReserach_Action", "AA", new { controller = "AResearch", action = "AA" });
-            routes.MapRoute("AReserach_Result", "AR", new { controller = "AResearch", action = "AR" });
-            routes.MapRoute("AReserach_Exception", "AE", new { controller = "AResearch", action = "AE" });
-            
-            routes.MapRoute("CHReserach_AD", "AD", new { controller = "CHResearch", action = "AD" });
-            routes.MapRoute("CHReserach_AP", "AP", new { controller = "CHResearch", action = "AP" });
-
+            routes.MapRoute("Default", "{controller}/{action}", new { controller = "AResearch", action = "AA" })
             routes.MapRoute("MXX", "MResearch/MXX", new { controller = "MResearch", action = "MXX" });
         }
     }
