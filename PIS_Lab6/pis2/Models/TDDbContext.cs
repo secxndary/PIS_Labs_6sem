9a1d50417f3bbd219d9e556721f0167d88142b47
alexander valdaitsevv <valdaitsevv@mail.ru>
2023-05-25 23:21:03 +0300

    clean folders

---
+++ PIS_Lab6/pis2/Models/TDDbContext.cs
@@ -0,0 +1,24 @@
+﻿using pis2.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace pis2.Models
+{
+    public class TDDbContext : DbContext, IDbContext
+    {
+
+        public DbSet<Phone> Phone { get; set; }
+
+        public TDDbContext() : base("name=MyConnectionString")
+        {
+        }
+
+        void IDbContext.SaveChanges()
+        {
+            SaveChanges();
+        }
+    }
+}
