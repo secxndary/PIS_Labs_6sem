0872526897c536e6b86a680440087a414b706da9
alexander valdaitsevv <53194830+secxndary@users.noreply.github.com>
2023-09-13 09:05:06 +0300

    Delete pis4 directory

--- pis4/pis2/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="pis2.MvcApplication" Language="C#" %>
--- pis4/pisda5b4/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace pisda5b4.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
